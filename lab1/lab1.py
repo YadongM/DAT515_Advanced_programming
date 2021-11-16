78f8d4575a64c94aa6e34a48057b8f1547839690
maodreamer <maodreamer@gmail.com>
2021-11-16 23:05:32 +0100

    Add dict function

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,7 +2,7 @@
 
 import json
 
-json_path = '../data/tramstops.json'
+
 def build_tram_stops(jsonobject):
     with open(jsonobject, 'r', encoding='utf-8') as f:
         temp = json.loads(f.read())
@@ -11,25 +11,60 @@ def build_tram_stops(jsonobject):
         info_stop = dict()
         for i in stop_key:
             info_stop[i] = dict()
-            info_stop[i]['lat'] = float(temp[i]['position'][0])
-            info_stop[i]['lon'] = float(temp[i]['position'][1])
-    
+            info_stop[i]['lat'] = temp[i]['position'][0]
+            info_stop[i]['lon'] = temp[i]['position'][1]
     return info_stop
 
 
-lines_path = '../data/tramstops.txt'
-def build_tram_lines(txt_path):
+
+def build_tram_lines(lines_path):
     with open(lines_path, "r", encoding='utf-8') as f:
         tram_number = []
         tram_stop = []
+        tram_time = []
         for i in f:
             if len(i)==3 or len(i) ==4:
                 tram_number.append(i[:-2])
                 tmp = []
                 tram_stop.append(tmp)
-            if len(i)>6:
-                tmp.append(i[:-6].rstrip())
-    return dict(zip(tram_number, tram_stop))
 
-def build_tram_times(txt_path):
-    tram_lines = build_tram_lines(txt_path)
+                time_tmp = []
+                tram_time.append(time_tmp)
+
+            if len(i)>6:
+                tmp.append(i[:-6].replace(" ", ""))
+        
+                time_tmp.append( list(map(int, (i[-6:].replace("\n", "")).split(':')))      )
+
+        
+    tram_lines = dict(zip(tram_number, tram_stop))
+    tram_time = dict(zip(tram_number, tram_time))
+
+    all_stop = list( set( [j for i in tram_lines.keys() for j in tram_lines[i]] ) )
+    all_stop = dict(zip(all_stop, [i for i in range(len(all_stop))]))
+
+
+    stop_time = dict()
+    for i in tram_lines.keys():
+        for j in range(len(tram_lines[i])):
+            if j <= (len(tram_lines[i])-2):
+                tmp = dict()
+                tmp[tram_lines[i][j+1]] = (tram_time[i][j+1][0] - tram_time[i][j][0])*60 + (tram_time[i][j+1][1] - tram_time[i][j][1])
+                stop_time[tram_lines[i][j]] = tmp
+            
+            if j <= (len(tram_lines[i])-2):
+                j = j+1
+                tmp = dict()
+                tmp[tram_lines[i][-j-1]] = (tram_time[i][-j][0] - tram_time[i][-j-1][0])*60 + (tram_time[i][-j][1] - tram_time[i][-j-1][1])
+                stop_time[tram_lines[i][-j]] = tmp
+    
+    return tram_lines,stop_time
+
+def build_tram_network(jsonobject, lines_path):
+    info_stop = build_tram_stops(jsonobject)
+    tram_lines,stop_time  = build_tram_lines(lines_path)
+    tramnetwork = {'stops':info_stop, 'lines':tram_lines, 'times':stop_time}
+    return tramnetwork
+
+
+
